d29917cb26ab1df1131585274c243a65c80f7c0e
Matin Tamizi <mtamizi@gmail.com>
2011-02-16 17:34:50 -0800

    dir structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='Poundpay',
+    version='0.0.1',
+    url='https://dev.poundpay.com/',
+    license='BSD',
+    author='Matin Tamizi',
+    author_email='devsupport@poundpay.com',
+    description='Payments platform for marketplaces',
+    packages=['poundpay'],
+)
