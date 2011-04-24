e2acb4b7da50172f7bec2b1627cb3b8ed71fdad5
Jacob Rus <jacobolus@gmail.com>
2011-04-23 18:51:23 -0700

    port 'escrow', 'release', 'cancel' methods on payment objects from ruby client

--- poundpay/payments.py
+++ poundpay/payments.py
@@ -1,5 +1,43 @@
 from .resource import Resource
 
+class PaymentError(Exception):
+    pass
+
+class PaymentEscrowError(PaymentError):
+    pass
+
+class PaymentReleaseError(PaymentError):
+    pass
+
+class PaymentCancelError(PaymentError):
+    pass
+
 
 class Payment(Resource):
     _name = 'payments'
+    
+    def escrow(self):
+        if self.status != 'AUTHORIZED':
+            raise PaymentEscrowError(
+                "Payment status is %s.  " % self.status +
+                "Only AUTHORIZED payments may be released")
+        
+        self.status = 'ESCROWED'
+        self.save()
+
+    def release(self):
+        if self.status != 'ESCROWED':
+            raise PaymentReleaseError(
+                "Payment status is %s.  " % self.status +
+                "Only ESCROWED payments may be released")
+        self.status = 'RELEASED'
+        self.save()
+
+    def cancel(self):
+        if self.status != 'ESCROWED':
+            raise PaymentCancelError(
+                "Payment status is %s.  " % self.status +
+                "Only ESCROWED payments may be canceled")
+
+        self.status = 'CANCELED'
+        self.save()
