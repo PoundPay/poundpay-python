c095e56c63021aa217ee17140110e199a5b4e228
Mahmoud Abdelkader <mabdelkader@gmail.com>
2011-04-25 22:18:12 -0700

    added test skeletons for developer, payments, and resource. fleshed out more the client tests

---
+++ tests/test_resource.py
@@ -0,0 +1,38 @@
+import unittest
+
+import mock
+
+from poundpay.resource import Resource
+
+
+class TestResource(unittest.TestCase):
+
+    def test_resource_constructor_updates__dict__(self):
+        pass
+
+    def test_repr(self):
+        pass
+
+    def test_all(self):
+        pass
+
+    def test_find(self):
+        pass
+
+    def test_save_when_payment_has_no_id_issues_POST(self):
+        pass
+
+    def test_save_when_payment_has_no_sid_issues_POST(self):
+        pass
+
+    def test_save_when_payment_has_an_id_issues_PUT(self):
+        pass
+
+    def test_save_when_payment_has_an_sid_issues_PUT(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_get_path(self):
+        pass
