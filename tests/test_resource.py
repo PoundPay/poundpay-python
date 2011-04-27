544755686151fb13ffc9138c0e56dc3ee0b94b76
Mahmoud Abdelkader <mabdelkader@gmail.com>
2011-04-26 19:20:52 -0700

    initial resource test flesh outs

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -8,13 +8,30 @@ from poundpay.resource import Resource
 class TestResource(unittest.TestCase):
 
     def test_resource_constructor_updates__dict__(self):
-        pass
+        resource_args = {'sid': 1, 'blah': 2}
+        resource = Resource(**resource_args)
+        self.assertEqual(resource.sid, 1)
+        self.assertEqual(resource.blah, 2)
+        self.assertEqual(resource.__dict__, resource_args)
 
     def test_repr(self):
-        pass
+        resource_args = {'sid': 1}
+        resource = Resource(**resource_args)
+        self.assertEqual('Resource(sid=1)', '%r' % resource)
 
     def test_all(self):
-        pass
+        resource_args = {'name': [{'sid': 1}]}
+        mock_client = mock.Mock()
+        mock_response = mock.Mock()
+        mock_response.json = resource_args
+        mock_client.get.return_value = mock_response
+        Resource.client = mock_client
+        Resource._name = 'name'
+        resources = Resource.all()
+        self.assertTrue(isinstance(resources, list))
+        self.assertTrue(isinstance(resources[0], Resource))
+        self.assertEqual(resources[0].sid, 1)
+        mock_client.get.assert_called_once_with('name')
 
     def test_find(self):
         pass
