a9a3030d055068451693b20816d6df50c1171b78
Andrew <andrew@poundpay.com>
2011-12-09 16:52:50 -0800

    .

--- examples/marketplace/config.py
+++ examples/marketplace/config.py
@@ -1,38 +1,10 @@
 IFRAME_ROOT_URI = 'https://www-sandbox.poundpay.com'
 
-DEV = dict(
-    credentials=dict(
-        developer_sid='DVb956191e242111e08016123140005921',
-        auth_token='3af5f79f0672f203cbabc9b75f8bd6cc379f61409092bd514709ccb6721d970d',
-        api_url='http://localhost:8000',
-        ),
-    IFRAME_ROOT_URI='http://localhost:5000',
-    )
 
 SANDBOX = dict(
     credentials=dict(             
-        developer_sid='DV1adf7e34140711e1b97212314000d16f',
-        auth_token='2b62669d5dc666113e06036c026624e1277171275c318965ed0d8d343fc02b09',
-        api_url='https://api-sandbox.poundpay.com',
-        ),
-    IFRAME_ROOT_URI='https://www-sandbox.poundpay.com',
-    )
-
-
-PROD = dict(
-    credentials=dict(             
-        developer_sid='DVb956191e242111e08016123140005921',
-        auth_token='3af5f79f0672f203cbabc9b75f8bd6cc379f61409092bd514709ccb6721d970d',
-        api_url='https://api.poundpay.com',
-        ),
-    IFRAME_ROOT_URI='https://www.poundpay.com',
-    )
-
-
-DOCS = dict(
-    credentials=dict(             
-        developer_sid='DV1adf7e34140711e1b97212314000d16f',
-        auth_token='2b62669d5dc666113e06036c026624e1277171275c318965ed0d8d343fc02b09',
+        developer_sid='DVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        auth_token='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',                     
         api_url='https://api-sandbox.poundpay.com',
         ),
     IFRAME_ROOT_URI='https://www-sandbox.poundpay.com',
@@ -40,12 +12,12 @@ DOCS = dict(
 
 
 DEFAULT_PAYMENT = dict(
-    amount='111',  # in usd cents
-    payer_fee_amount='13',  # in usd cents
-    recipient_fee_amount='32',  # in usd cents
-    payer_email_address='andrew+hi@poundpay.com',
-    recipient_email_address='andrew+bye@poundpay.com',
-    description='Some Nutti Junk',
+    amount='20011',  # in usd cents
+    payer_fee_amount='123',  # in usd cents
+    recipient_fee_amount='321',  # in usd cents
+    payer_email_address='fred@example.com',
+    recipient_email_address='immanuel@example.com',
+    description='Beats by Dr. Dre (White)',
     developer_identifier='',
     )
 
@@ -57,9 +29,6 @@ def get_credentials_for_env(environment):
     global IFRAME_ROOT_URI
     configurations = {
         'SANDBOX': SANDBOX,
-        'DOCS': DOCS,
-        'DEV': DEV,
-        'PROD': PROD,
         }
     environment = environment.upper()
     configuration = configurations[environment]
